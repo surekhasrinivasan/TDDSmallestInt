279d1710b79d25adc5f12cc917cf54bdb3f1f46c
surekhasrinivasan <surekhasrao@gmail.com>
2018-11-13 10:51:38 -0500

    TDD Smallest Int - Part I completed - refactored code

--- SmallestService.Test/UnitTest1.cs
+++ SmallestService.Test/UnitTest1.cs
@@ -7,10 +7,18 @@ namespace Tests
 {
     public class Tests
     {
+        private Finder finder;
+
+        [SetUp]
+        public void SetUpFinderTests()
+        {
+            finder = new Finder();
+        } 
+
         [Test]
         public void ThrowsExceptionIfArrayInputIsEmpty()
         {
-            var finder = new Finder();
+            //var finder = new Finder();
             var input = new int[0];
             Assert.Throws<System.ArgumentException>(() => finder.FindSmallestInt(input));
         }
@@ -18,7 +26,7 @@ namespace Tests
         [Test]
         public void IfArrayLengthIsOneReturnInt()
         {
-            var finder = new Finder();
+            //var finder = new Finder();
             var input = new int[1] { 1 };
             var output = finder.FindSmallestInt(input);
             bool returnInt = (output.GetType() == typeof(int));
@@ -29,7 +37,7 @@ namespace Tests
         [Test]
         public void IfArrayLengthIsTwoReturnLowestInt()
         {
-            var finder = new Finder();
+            //var finder = new Finder();
             var input = new int[2] { 3, 1 };
             var output = finder.FindSmallestInt(input);
             var expected = 1;
@@ -40,7 +48,7 @@ namespace Tests
         [Test]
         public void IfArrayLengthIsThreeReturnLowestInt()
         {
-            var finder = new Finder();
+            //var finder = new Finder();
             var input = new int[3] { 5, 3, 1 };
             var output = finder.FindSmallestInt(input);
             var expected = 1;
@@ -51,7 +59,7 @@ namespace Tests
         [Test]
         public void IfArrayLengthIsGreaterThanZeroReturnSmallestInt()
         {
-            var finder = new Finder();
+            //var finder = new Finder();
             var input = new int[7] { 2, 5, 3, 1, 6, 9, 4 };
             var output = finder.FindSmallestInt(input);
             var expected = 1;
