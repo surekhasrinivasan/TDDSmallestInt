d19c1ff5244a9b5d8d51cb0e474faf63b036dac4
surekhasrinivasan <surekhasrao@gmail.com>
2018-11-14 06:33:36 -0500

    TDD Smallest Int - Part 2 completed

--- SmallestService.Test/UnitTest2.cs
+++ SmallestService.Test/UnitTest2.cs
@@ -1,23 +1,69 @@
-//using NUnit.Framework;
-//using SmallestService;
-//using System;
+using NUnit.Framework;
+using SmallestService;
+using System;
 
-//namespace Tests
-//{
-//    public class Tests2
-//    {
-//        [Test]
-//        public void ThrowsExceptionIfArrayInputIsEmpty()
-//        {
-//            var finder = new Finder();
-//            var input = new int[0];
-//            Assert.Throws<System.ArgumentException>(() => finder.FindClosestIntToZero(input));
-//        }
+namespace Tests
+{
+    public class Tests2
+    {
+        private Finder finder;
 
-//        [Test]
-//        public void FindClosestIntToZeroIfArrayLengthIsOneAndInputIsOneReturnOne()
-//        {
+        [SetUp]
+        public void SetUpFinderTests()
+        {
+            finder = new Finder();
+        }
 
-//        }
-//    }
-//}
+        [Test]
+        public void ThrowsExceptionIfArrayInputIsEmpty()
+        {
+            var input = new int[0];
+            Assert.Throws<System.ArgumentException>(() => finder.FindClosestIntToZero(input));
+        }
+
+        [Test]
+        public void FindClosestIntToZeroIfArrayLengthIsOneAndInputIsOneReturnOne()
+        {
+            var input = new int[1] { 1 };
+            var output = finder.FindClosestIntToZero(input);
+            var expected = 1;
+            Assert.AreEqual(expected, output);            
+        }
+
+        [Test]
+        public void FindClosestIntToZeroIfInputIsTwoAndThreeReturnsTwo()
+        {
+            var input = new int[2] { 2, 3 };
+            var output = finder.FindClosestIntToZero(input);
+            var expected = 2;
+            Assert.AreEqual(expected, output);
+        }
+
+        [Test]
+        public void IfInputLengthIsThreeReturnsClosestToZero()
+        {
+            var input = new int[3] { 5, 2, 3 };
+            var output = finder.FindClosestIntToZero(input);
+            var expected = 2;
+            Assert.AreEqual(expected, output);            
+        }
+
+        [Test]
+        public void IfInputContainsNegativeNumberReturnClosestToZero()
+        {
+            var input = new int[3] { -3, -5, -2 };
+            var output = finder.FindClosestIntToZero(input);
+            var expected = -2;
+            Assert.AreEqual(expected, output);            
+        }
+
+        [Test]
+        public void IfInputContainsNegativeNumberAndPostiveNumberBothAreSameClosestToZeroReturnNegativeNumber()
+        {
+            var input = new int[4] { -2, 4, -1, 1 };
+            var output = finder.FindClosestIntToZero(input);
+            var expected = -1;
+            Assert.AreEqual(expected, output);
+        }
+    }
+}
--- SmallestService/Class1.cs
+++ SmallestService/Class1.cs
@@ -7,12 +7,37 @@ namespace SmallestService
     {
         public int FindSmallestInt(int[] input)
         {
-            if(input.Length < 1)
+            if (input.Length == 0)
             {
                 throw new ArgumentException();
             }
             Array.Sort(input);
-            return input[0];            
+            return input[0];
+        }
+
+        public int FindClosestIntToZero(int[] input)
+        {
+            int num = int.MaxValue;
+
+            if (input.Length == 0)
+            {
+                throw new ArgumentException();
+            }
+
+            foreach (int i in input)
+            {
+
+                if (Math.Abs(i) < Math.Abs(num))
+                {
+                    num = i;
+                }
+
+                if ((Math.Abs(i) == Math.Abs(num)) && (i < num))
+                {
+                    num = i;
+                }                
+            }
+            return num;
         }
     }
 }
